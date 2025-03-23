fb7e41918fc96e9ec41ef8af347f86788b9f3fe9
Armel <armelbedjra12@gmail.com>
2025-03-23 23:07:28 +0000

    Description

--- src/main/java/com/eschoolback/eschool/repository/TitulaireRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eschoolback.eschool.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TitulaireRepository extends JpaRepository<Titulaire, Long> {
-}
